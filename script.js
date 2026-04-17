fd9975da305b2a28c4408d5f1aaed0115bed0a47
Ashita <ashitathakur2807@gmail.com>
2026-04-17 18:54:42 +0530

    added local storage features

--- script.js
+++ script.js
@@ -1,39 +1,92 @@
+// saving task on page onload
+window.onload = function () {
+  loadTask();
+};
+
+// Add task function
 function addTask() {
   let input = document.getElementById("taskInput");
   let task = input.value;
-  //   console.log(task);
+
   if (task === "") {
     alert("Please enter a task.");
     return;
   }
 
-  let li = document.createElement("li");
-
-  li.innerText = task;
-  //   console.log(li);
-
-  li.onclick = function () {
-    if (li.style.textDecoration === "line-through") {
-      li.style.textDecoration = "none";
-    } else {
-      li.style.textDecoration = "line-through";
-    }
-    // li.style.textDecoration = "line-through";
-    // console.log("clicked");
-  };
-
-  let deletebtn = document.createElement("button");
-  //   console.log(deletebtn);
-  deletebtn.innerText = "Delete";
-
-  deletebtn.onclick = function () {
-    li.remove();
-  };
-  li.appendChild(deletebtn);
-  document.getElementById("taskList").appendChild(li);
+  createTaskElement(task);
+  saveTask(task);
   input.value = "";
+
+  //Task element creation
+  function createTaskElement(task) {
+    let li = document.createElement("li");
+    li.innerText = task;
+  }
+
+  //Complete Toggle
+  li.onclick = function () {
+    li.classList.toggle("completed");
+  };
+
+  // let li = document.createElement(task);
+
+  // li.innerText = task;
+  // //   console.log(li);
+
+  // li.onclick = function () {
+  //   if (li.style.textDecoration === "line-through") {
+  //     li.style.textDecoration = "none";
+  //   } else {
+  //     li.style.textDecoration = "line-through";
+  //   }
+  //   // li.style.textDecoration = "line-through";
+  //   // console.log("clicked");
+  // };
+
+  // Delete Button
+  let deleteBtn = document.createElement("button");
+  //   console.log(deletebtn);
+  deleteBtn.innerText = "Delete";
+
+  deleteBtn.onclick = function (e) {
+    e.stopPropagation();
+    li.remove();
+    removeTask(task);
+  };
+
+  li.appendChild(deleteBtn);
+
+  document.getElementById("taskList").appendChild(li);
 }
 
+// Save Task to local Storage
+function saveTask(task) {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.Stringify(tasks));
+}
+
+// Load Task
+function loadTask() {
+  function loadTask() {
+    JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(function (task) {
+      createTaskElement(task);
+    });
+  }
+}
+
+//Remove Task
+function removeTask(taskToDelete) {
+  let task = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks = tasks.filter(function (task) {
+    return task !== taskToDelete;
+  });
+
+  localStorage.setItems("tasks", JSON.stringify(tasks));
+}
+
+// Enter key Support
 document
   .getElementById("taskInput")
   .addEventListener("keypress", function (event) {
